625c2ee570b9b72fbe3c4da3759edb438ed7e7d4
Jensone <hello@jensone.com>
2023-04-05 16:52:34 +0200

    🐛 fix a bug

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -22,23 +22,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin', methods: ['GET', 'POST'])]
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin_index.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($adminUrlGenerator->setController(BookCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -47,34 +36,19 @@ class DashboardController extends AbstractDashboardController
             // the name visible to end users
             ->setTitle('Biblio App')
 
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
             // ->renderContentMaximized()
 
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
             // ->renderSidebarMinimized()
 
-            // by default, users can select between a "light" and "dark" mode for the
-            // backend interface. Call this method if you prefer to disable the "dark"
-            // mode for any reason (e.g. if your interface customizations are not ready for it)
             ->disableDarkMode()
 
-            // set this option if you want to enable locale switching in dashboard.
-            // IMPORTANT: this feature won't work unless you add the {_locale}
-            // parameter in the admin dashboard URL (e.g. '/admin/{_locale}').
-            // the name of each locale will be rendered in that locale
-            // (in the following example you'll see: "English", "Polski")
             ->setLocales(['en', 'fr'])
-            // to customize the labels of locales, pass a key => value array
-            // (e.g. to display flags; although it's not a recommended practice,
-            // because many languages/locales are not associated to a single country)
+            
             ->setLocales([
                 'en' => '🇬🇧 English',
                 'fr' => '🇫🇷 Français'
             ])
-            // to further customize the locale option, pass an instance of
-            // EasyCorp\Bundle\EasyAdminBundle\Config\Locale
+            
             ->setLocales([
                 'en', // locale without custom options
                 Locale::new('fr', 'french', 'far fa-language') // custom label and icon
